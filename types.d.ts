9231280fed2a6d159eee96f05617eab9887f22b3
thepaperpilot <thepaperpilot@gmail.com>
2022-09-04 17:49:59 -0500

    Setup placeholder interface properties

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,22 @@
 interface ContentPack {
-
+    nodeTypes: {
+        [x: string]: {
+            name: string;
+        }
+    }
 }
 
 interface GameState {
-
+    layers: {
+        world: {
+            nodes: {
+                position: {
+                    x: number;
+                    y: number;
+                },
+                type: string;
+                data: unknown;
+            }[];
+        }
+    }
 }
